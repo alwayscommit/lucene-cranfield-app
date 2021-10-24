cc30bf567b04d19074e04a8f9a0114998b9d6a76
aakash ranglani <ranglaniaakash.git@gmail.com>
2021-10-24 10:59:41 +0100

    shell script to evaluate output

--- evaluate.sh
+++ evaluate.sh
@@ -1,2 +1,18 @@
-echo "'hello', 'world'" >> log.csv
-for FILE in *; do echo -e "$FILE\nLoops Rule\!" > $FILE; done
+rm -rf precision.txt
+rm -rf recall.txt
+rm -rf map.txt
+rm -rf gm_map.txt
+
+
+for FILE in 'output/'*; 
+do
+	echo $FILE
+	echo $FILE >> precision.txt
+	trec_eval/trec_eval -m P ./cranfield/QRelsCorrectedforTRECeval $FILE >> precision.txt
+	echo $FILE >> recall.txt
+	trec_eval/trec_eval -m recall ./cranfield/QRelsCorrectedforTRECeval $FILE >> recall.txt
+	echo $FILE >> map.txt
+	trec_eval/trec_eval -m map ./cranfield/QRelsCorrectedforTRECeval $FILE >> map.txt
+	echo $FILE >> gm_map.txt
+	trec_eval/trec_eval -m gm_map ./cranfield/QRelsCorrectedforTRECeval $FILE >> gm_map.txt
+done
